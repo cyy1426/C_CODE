a759fdd9c25c5e2c7cbd3e279538647ff636afa8
cyy1426 <1142154611@qq.com>
2020-04-12 22:44:59 +0800

    每日一题之检查数组中元素位数为偶数的个数

---
+++ test_4_12/test_4_12.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+void oushi(int* arr, int sz)
+{
+	int i = 0;
+	int count=0;
+	for (i = 0; i < sz ; i++)
+	{
+		int number = 1;
+		while (arr[i] / 10 != 0)
+		{
+			arr[i] = arr[i] / 10;
+			number++;
+		}
+		if (number % 2 == 0)
+			count++;
+		
+	}
+	printf("%d", count);
+}
+int main()
+{
+	int arr[] = { 15,56,4545,456 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	oushi(arr, sz);
+}
