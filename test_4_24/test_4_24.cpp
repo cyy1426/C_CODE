979816985b8f025dcb77a167ef8584e229f2e19e
cyy1426 <1142154611@qq.com>
2020-04-24 11:13:37 +0800

    模拟实现memmove 实现重叠数组的复制

---
+++ test_4_24/test_4_24.cpp
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<assert.h>
+void* my_memmove(void* dest, const void* src, size_t num)
+{
+	assert(dest && src);
+	void* temp = dest;
+	if (dest < src)
+	{
+		while (num--)
+		{
+			*(char*)dest = *(char*)src;
+			dest = (char*)dest + 1;
+			src = (char*)src + 1;
+		}
+	}
+	else
+	{
+		while (num--)
+		{
+			*((char*)dest + num) = *((char*)src + num);
+		}
+
+	}
+	return temp;
+}
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7,8,9 };
+	my_memmove(arr1, arr1+2, 5);
+	long d = 9;
+	printf("%d", sizeof(d));
+}
