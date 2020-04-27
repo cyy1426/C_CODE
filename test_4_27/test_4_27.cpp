29a7e81a8d17bf4e116f90f2b395f3f5fde5f673
cyy1426 <1142154611@qq.com>
2020-04-27 23:22:54 +0800

    查询1的位数

---
+++ test_4_27/test_4_27.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+int rob(int* nums, int numsSize) {
+
+	int i = 0;
+	int sum1 = 0, sum2 = 0;
+	while (i < numsSize)
+	{
+		sum1 = sum1 + nums[i];
+		sum2 = sum2 + nums[i + 1];
+		i = i + 2;
+	}
+	if (sum1 > sum2)
+	{
+		return sum1;
+	}
+	else
+		return sum2;
+
+}
+int main()
+{
+	int arr[] = {2,7,9,3,1 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = rob(arr, sz);
+	printf("%d", ret);
+}
