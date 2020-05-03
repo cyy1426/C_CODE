93a50acc38698ce3f4162a2608ddab938019f803
cyy1426 <1142154611@qq.com>
2020-05-03 19:47:18 +0800

    遍历一个棋盘就是二维数组，俩for循环即可遍历，然后在一个位置上下左右去找值这一个变量for循环即可

---
+++ test_5_3/test_5_3.cpp
@@ -0,0 +1,59 @@
+﻿#include <stdio.h>
+int numRookCaptures(char** board, int boardSize, int* boardColSize) {
+	int X, Y;
+	int cnt = 0;
+	//第一个for循环去找车所在的位置赋值给X ，Y
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (board[i][j] == 'R') {
+				X = i;
+				Y = j;
+			}
+		}
+	}
+	//向左找p，有吃count加1，遇见B就break
+	for (int i = (Y - 1); i >= 0; i--) {
+		if (board[X][i] == 'p') {
+			cnt++;
+			break;
+		}
+		if (board[X][i] == 'B') {
+			break;
+		}
+	}
+	//向右找p，有吃count加1，遇见B就break
+	for (int i = (Y + 1); i < 8; i++) {
+		if (board[X][i] == 'p') {
+			cnt++;
+			break;
+		}
+		if (board[X][i] == 'B') {
+			break;
+		}
+	}
+	//向上找p，有吃count加1，遇见B就break
+	for (int i = (X - 1); i >= 0; i--) {
+		if (board[i][Y] == 'p') {
+			cnt++;
+			break;
+		}
+		if (board[i][Y] == 'B') {
+			break;
+		}
+	}
+	//向下找p，有吃count加1，遇见B就break
+	for (int i = (X + 1); i < 8; i++) {
+		if (board[i][Y] == 'p') {
+			cnt++;
+			break;
+		}
+		if (board[i][Y] == 'B') {
+			break;
+		}
+	}
+	return cnt;
+}
+int main()
+{
+
+}
