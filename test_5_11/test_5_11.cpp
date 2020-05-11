3c566665d03aaed7e68f5cb3ba3ef69d58675892
cyy1426 <1142154611@qq.com>
2020-05-11 21:57:34 +0800

    一个牛皮的二进制转十进制的思路！！！

---
+++ test_5_11/test_5_11.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	//char buf[1024] = {0};
+//	//FILE* pf = fopen("test.txt", "w");
+//	//if (pf == NULL)
+//	//{
+//	//	return 0;
+//	//}
+//	////写文件
+//	//fputs("hello\n",pf);
+//	//fputs("world\n", pf);
+//	//
+//	//fclose(pf);
+//	//pf = NULL;
+//	char buf[1024] = {0};
+//	fgets(buf);
+//	fputs(buf);
+//
+//	return 0;
+//}
+bool* prefixesDivBy5(int* A, int ASize, int* returnSize)
+{
+	bool* p = (bool*)malloc(sizeof(bool)*ASize);
+	int i = 0;
+	*returnSize = ASize;
+	int temp = 0;
+	for (i = 0; i < ASize; i++)
+	{
+		temp = temp * 2 + A[i];
+		if (temp % 5 == 0)
+			p[i] = true;
+		else
+			p[i] = false;
+		temp = temp % 10;
+
+	}
+	return p;
+}
