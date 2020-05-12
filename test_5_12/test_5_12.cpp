1b0c047a35ba4838eaa6c4f74138103d4b095365
cyy1426 <1142154611@qq.com>
2020-05-12 22:09:45 +0800

    二分法查找数组的元素返回下标！

---
+++ test_5_12/test_5_12.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+//一个二分法查找在数组中查询所查找数的下标然后返回
+int two(int* arr, int len, int b)
+{
+	int left = 0, right = len - 1;//定义左右下标
+	while (left <= right)
+	{
+		int ret = (left + right) / 2;//二分法取左加右的一半！！
+		if (b == arr[ret])
+			return ret;
+		else if (b > arr[ret])
+			left = ret + 1;
+		else
+			right = ret - 1;
+	}
+	return -1;
+}
+int main()
+{
+	int arr[] = {1,3,5,6,7,9};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int b = 6;
+	int c=two(arr, sz, b);
+	printf("%d",c);
+}
