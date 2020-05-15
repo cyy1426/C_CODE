1d63203052a75db44d6d5a4addc9ef8811fbed08
cyy1426 <1142154611@qq.com>
2020-05-15 21:58:24 +0800

    一个递归小题求前两项之和！

---
+++ test_5_15/test_5_15.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int fib(int N) {
+	if (N < 1)
+		return 0;
+	else if (N == 1)
+		return 1;
+	else
+		return fib(N - 1) + fib(N - 2);
+
+}
+int main()
+{
+	int a = 0;
+	scanf("%d",&a);
+	printf("%d", fib(a));
+}
