d8f52b808a0181765bc0bddf079f5fedd262f69d
cyy1426 <1142154611@qq.com>
2020-05-22 00:00:47 +0800

    一个牛皮的思路去除数组中重复元素！

---
+++ test_5_21/test_5_21.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int num(int* arr, int sz)
+{
+	int i = 0, count = 0;
+	for (i = 0; i < sz; i++)
+	{
+		if (arr[count] != arr[i])//count是一个确定位置的信息  不等就复制  等的话不进去   1  1 2 2  -》 1 2 2 2
+		{
+			arr[count++] = arr[i];
+		}
+	}
+	return ++count;
+}
+int main()
+{
+	int arr[] = {0,1,2,2};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	printf("%d",num(arr,sz));
+}
