0a73a3f744fc0979006bb373b9fbc119e043b32c
cyy1426 <1142154611@qq.com>
2020-06-10 23:13:21 +0800

    翻转链表！

---
+++ test_6_10/test_6_10.cpp
@@ -0,0 +1,20 @@
+﻿///反转链表
+
+struct ListNode* reverseList(struct ListNode* head) {
+	if (head == NULL)
+		return NULL;
+	if (head->next == NULL)
+		return head;
+	struct ListNode* cur = head;
+	struct ListNode* pre = NULL;
+	struct ListNode* next = cur->next;
+	while (next)
+	{
+		next = cur->next;
+		cur->next = pre;
+		pre = cur;
+		cur = next;
+	}
+	return pre;
+
+}
