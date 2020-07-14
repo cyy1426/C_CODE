74d8e40488855b7cddf91f64144ea65d6ccf12c5
cyy1426 <1142154611@qq.com>
2020-07-15 00:01:57 +0800

    一个链表的插入排序，先找插入位置，比较插入！

---
+++ test_7_14/test_7_14.cpp
@@ -0,0 +1,47 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* insertionSortList(struct ListNode* head) {
+	if (head == NULL || head->next == NULL)
+		return head;
+	struct ListNode* newhead = NULL;
+	struct ListNode* cur = head;
+	while (cur)
+	{
+		head = cur->next;
+		//新链表中有结点 找新位置
+		struct ListNode* insertpos = newhead;
+		struct ListNode* insertprev = NULL;
+		while (insertpos)
+		{
+			if (cur->val > insertpos->val)
+			{
+				insertprev = insertpos;
+				insertpos = insertpos->next;
+
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (NULL == insertprev)
+		{
+			cur->next = newhead;
+			newhead = cur;
+		}
+		else
+		{
+			cur->next = insertpos;
+			insertprev->next = cur;
+		}
+		cur = head;
+	}
+	return newhead;
+}
